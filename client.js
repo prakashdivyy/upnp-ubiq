0f0d2158ddc0e74ddd7478239827ba65e3e73465
prakashdivyy <prakashdivyy@gmail.com>
2016-12-30 19:50:49 +0700

    Add door and sea

--- client.js
+++ client.js
@@ -6,13 +6,22 @@ const sleep = require('sleep');
 
 let app = express();
 
-app.get('/', function (req, res) {
+app.get('/seat', function (req, res) {
     let client = new Client();
     client.on('response', function (headers, statusCode, rinfo) {
         let dev = headers.LOCATION;
         res.send(dev);
     });
     client.search('urn:schemas-upnp-org:device:SeatManager:1');
+});
+
+app.get('/door', function (req, res) {
+    let client = new Client();
+    client.on('response', function (headers, statusCode, rinfo) {
+        let dev = headers.LOCATION;
+        res.send(dev);
+    });
+    client.search('urn:schemas-upnp-org:device:SmartDoorManager:1');
 });
 
 app.listen(3000, function () {
