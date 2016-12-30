a80956448636f6c977481e19343f59b31c0db745
prakashdivyy <prakashdivyy@gmail.com>
2016-12-30 19:28:35 +0700

    Test test

--- client.js
+++ client.js
@@ -5,13 +5,15 @@ const Client = require('node-ssdp').Client;
 
 let client = new Client();
 let app = express();
+let a = 10;
 
 client.on('response', function (headers, statusCode, rinfo) {
-    console.log('Got a response to an m-search.');
+    a+=1;
 });
 
-app.get('/', function (req, res) {
-    client.search('ssdp:all');
+app.get('/SeatManager', function (req, res) {
+    client.search('urn:schemas-upnp-org:device:SeatManager:1');
+    res.send(a);
 });
 
 app.listen(3000, function () {
